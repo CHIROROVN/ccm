bb533c74659ab4dbcb261e8a17f715af7c88f478
phuong.nd@chiroro.com.vn <phuong.nd@chiroro.com.vn>
2017-12-12 16:23:25 +0700

    update

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -59,7 +59,6 @@
 
 @include('backend.layouts.sidebar')
 
-=======
 <div id="sidebar"><a href="#" class="visible-phone"><i class="icon icon-home"></i> Dashboard</a>
   <ul>
     <li class="active"><a href="index.html"><i class="icon icon-home"></i> <span>Dashboard</span></a> </li>
