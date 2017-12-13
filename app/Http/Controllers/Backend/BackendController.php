ee67c91776e8f8267b57d1b88f045e0acd89b2ee
phuong.nd@chiroro.com.vn <phuong.nd@chiroro.com.vn>
2017-12-13 14:28:46 +0700

    update

--- app/Http/Controllers/Backend/BackendController.php
+++ app/Http/Controllers/Backend/BackendController.php
@@ -7,6 +7,24 @@ class BackendController extends Controller
     public function __construct(){
 
         //$this->middleware('auth', ['except' => ['postLogin', 'login','logout']]);
+
+         //get IP address from user
+        if (isset($_SERVER['HTTP_CLIENT_IP']))
+            $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+        else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        else if(isset($_SERVER['HTTP_X_FORWARDED']))
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+        else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+            $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+        else if(isset($_SERVER['HTTP_FORWARDED']))
+            $ipaddress = $_SERVER['HTTP_FORWARDED'];
+        else if(isset($_SERVER['REMOTE_ADDR']))
+            $ipaddress = $_SERVER['REMOTE_ADDR'];
+        else
+            $ipaddress = '';
+
+        define('CLIENT_IP_ADRS', $ipaddress);
     }
 
 }
