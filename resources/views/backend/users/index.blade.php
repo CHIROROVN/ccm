21fadcbf0b146d8aa93687ea7ce48ee592296a94
phuong.nd@chiroro.com.vn <phuong.nd@chiroro.com.vn>
2017-12-14 10:31:12 +0700

    update

--- resources/views/backend/users/index.blade.php
+++ resources/views/backend/users/index.blade.php
@@ -10,76 +10,73 @@
     <hr>
     <div class="row-fluid">
       <div class="span12">
-        <div class="widget-box">
-          <div class="widget-title"> <span class="icon">
-            <input type="checkbox" id="title-checkbox" name="title-checkbox" />
-            </span>
-            <h5>User list</h5>
+        <div class="flash-messages">
+          @if($message = Session::get('danger'))
+
+              <div id="error" class="message">
+                  <a id="close" title="Message"  href="javascript:void(0);" onClick="document.getElementById('error').setAttribute('style','display: none;');">&times;</a>
+                  <span>{{$message}}</span>
+              </div>
+
+          @elseif($message = Session::get('success'))
+
+              <div id="success" class="message">
+                  <a id="close" title="Message"  href="javascript:void(0);" onClick="document.getElementById('success').setAttribute('style','display: none;');">&times;</a>
+                  <span>{{$message}}</span>
+              </div>
+
+          @endif  
+        </div>
+       <div class="widget-box">
+          <div class="widget-title"> <span class="icon"> <i class="icon-th"></i> </span>
+            <h5>Static table</h5>
           </div>
           <div class="widget-content nopadding">
-            <table class="table table-bordered table-striped with-check">
+            <table class="table table-bordered table-striped">
               <thead>
                 <tr>
-                  <th><i class="icon-resize-vertical"></i></th>
                   <th>Rendering engine</th>
                   <th>Browser</th>
                   <th>Platform(s)</th>
                   <th>Engine version</th>
+                  <th>CSS grade</th>
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td><input type="checkbox" /></td>
+                <tr class="odd gradeX">
                   <td>Trident</td>
                   <td>Internet
                     Explorer 4.0</td>
                   <td>Win 95+</td>
                   <td class="center"> 4</td>
+                  <td class="center">X</td>
                 </tr>
-                <tr>
-                  <td><input type="checkbox" /></td>
+                <tr class="even gradeC">
                   <td>Trident</td>
                   <td>Internet
                     Explorer 5.0</td>
                   <td>Win 95+</td>
                   <td class="center">5</td>
+                  <td class="center">C</td>
                 </tr>
-                <tr>
-                  <td><input type="checkbox" /></td>
-                  <td>Trident</td>
-                  <td>Internet
-                    Explorer 4.0</td>
-                  <td>Win 95+</td>
-                  <td class="center"> 4</td>
-                </tr>
-                <tr>
-                  <td><input type="checkbox" /></td>
-                  <td>Trident</td>
-                  <td>Internet
-                    Explorer 5.0</td>
-                  <td>Win 95+</td>
-                  <td class="center">5</td>
-                </tr>
-                <tr>
-                  <td><input type="checkbox" /></td>
+                <tr class="odd gradeA">
                   <td>Trident</td>
                   <td>Internet
                     Explorer 5.5</td>
                   <td>Win 95+</td>
                   <td class="center">5.5</td>
+                  <td class="center">A</td>
                 </tr>
-                <tr>
-                  <td><input type="checkbox" /></td>
+                <tr class="even gradeA">
                   <td>Trident</td>
                   <td>Internet
                     Explorer 6</td>
                   <td>Win 98+</td>
                   <td class="center">6</td>
+                  <td class="center">A</td>
                 </tr>
               </tbody>
-            </table>
-          </div>
-        </div>
+            </table> 
 
       </div>
     </div>
--- resources/views/backend/users/login.blade.php
+++ resources/views/backend/users/login.blade.php
@@ -18,7 +18,7 @@
                 @if($message = Session::get('danger'))
 
                     <div id="error" class="message">
-                        <a id="close" title="Message"  href="#" onClick="document.getElementById('error').setAttribute('style','display: none;');">&times;</a>
+                        <a id="close" title="Message"  href="javascript:void(0);" onClick="document.getElementById('error').setAttribute('style','display: none;');">&times;</a>
                         <span>{{$message}}</span>
                     </div>
 
