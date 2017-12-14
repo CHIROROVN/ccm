dd3b2672514664d66934214727a7151e63da746d
hoaunet <hoaunet@yahoo.com>
2017-12-14 15:47:12 +0700

    Update

---
+++ app/Http/Models/ContractModel.php
@@ -0,0 +1,49 @@
+<?php 
+namespace App\Http\Models;
+use DB;
+use Validator;
+
+class ContractModel {   
+
+    protected $table = 'm_contract';
+    protected $primaryKey   = 'contract_id';
+
+   public function Rules()
+    {
+        return array(
+            'contract_no'                      => 'required',
+            //'contact_email'                      => 'required',
+        );
+    }
+
+    public function Messages()
+    {
+        return array(
+           'contract_no.required'             => trans('validation.error_contract_no_required'),
+           //'contact_email.required'             => trans('validation.error_contact_email_required'),
+           
+        );
+    }
+
+    public function get_all()
+    {
+        return DB::table($this->table)->leftJoin('m_company', 'm_company.company_id', '=', 'm_contract.company_id')
+                                     ->where('m_contract.last_kind', '<>', DELETE)->orderBy('contract_id', 'DESC')->get();
+    }
+
+    public function insert($data)
+    {
+        return DB::table($this->table)->insert($data);
+    }
+
+    public function update($id, $data)
+    {
+        return DB::table($this->table)->where('contract_id', $id)->update($data);
+    }
+
+    public function get_by_id($id)
+    {
+        return DB::table($this->table)->where('contract_id', $id)->first();
+    }
+
+}
