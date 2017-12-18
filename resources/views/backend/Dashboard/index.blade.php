2be28f387fa28ec23201cc1966b3fadb1195ca44
phuong.nd@chiroro.com.vn <phuong.nd@chiroro.com.vn>
2017-12-18 14:16:11 +0700

    update

--- resources/views/backend/Dashboard/index.blade.php
+++ resources/views/backend/Dashboard/index.blade.php
@@ -11,12 +11,25 @@
   <div class="container-fluid">
     <div class="quick-actions_homepage">
       <ul class="quick-actions">
-        <li class="bg_lb span3"> <a href="index.html"> <i class="icon-dashboard"></i> 
+        
+        <li class="bg_lb span2"> <a href="index.html"> <i class="icon-dashboard"></i>
           <!-- <span class="label label-important">20</span> --> Dashboard </a> </li>
-        <li class="bg_lg span2"> <a href="company.html"> <i class="icon-home"></i> Company</a> </li>        
-        <li class="bg_lo span2"> <a href="contract.html"> <i class="icon-book"></i> Contracts</a> </li>
-        <li class="bg_lb span2"> <a href="Meeting.html"> <i class="icon-group"></i> Meeting</a> </li>
-        <li class="bg_ls span2"> <a href="users.html"> <i class="icon-user"></i> Users</a> </li>
+       
+        @if(Auth::user()->u_power01 == 1)
+        <li class="bg_ly span2"> <a href="{{route('backend.company.index')}}"> <i class="icon-home"></i> Company</a> </li>
+        @endif
+         @if(Auth::user()->u_power02 == 1)
+        <li class="bg_lg span2"> <a href="{{route('backend.contact.index')}}"> <i class="icon-phone"></i> Contact</a> </li>
+        @endif
+        @if(Auth::user()->u_power03 == 1)       
+        <li class="bg_lo span2"> <a href="{{route('backend.contract.index')}}"> <i class="icon-book"></i> Contracts</a> </li>
+        @endif
+        @if(Auth::user()->u_power04 == 1)
+        <li class="bg_lb span2"> <a href="{{route('backend.meeting.index')}}"> <i class="icon-group"></i> Meeting</a> </li>
+        @endif
+        @if(Auth::user()->u_power05 == 1)
+        <li class="bg_ls span2"> <a href="{{route('backend.users.index')}}"> <i class="icon-user"></i> Users</a> </li>
+        @endif
 
       </ul>
     </div> 
